a8d735e5baa47d372879eb2488ef94e9467003ef
Caique Rodrigues <caiquerodrigues@id.uff.br>
2021-05-17 11:12:45 -0300

    cloneNode in the forms

--- public/scripts/admin-scripts.js
+++ public/scripts/admin-scripts.js
@@ -1,8 +1,24 @@
-const viewRecipe = document.querySelectorAll('.recipe-update')
+document.querySelector('.add-ingredient').addEventListener("click", () => {
+    const ingredients = document.querySelector('.ingredients')
+    const ingredient = document.querySelectorAll('.ingredients input')
+
+    const newIngredient = ingredient[ingredient.length - 1].cloneNode(true)
+
+    if(newIngredient.value == "") return false
+
+    newIngredient.value = ""
+    ingredients.appendChild(newIngredient)
+})
 
 
-for(let i=0; i <= viewRecipe.length; i++){
-    viewRecipe[i].addEventListener('click', () => {
-        window.location.href = `/recipes/${i+1}`
-    })
-}
+document.querySelector('.add-step').addEventListener('click', () => {
+    const preparations = document.querySelector('.preparations')
+    const preparationStep = document.querySelectorAll('.preparations input')
+
+    const newStep = preparationStep[preparationStep.length - 1].cloneNode(true)
+
+    if(newStep.value == "") return false
+
+    newStep.value = ""
+    preparations.appendChild(newStep)
+})
